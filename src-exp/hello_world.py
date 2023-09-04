334f03efa9a15e0c1064fdb548572153d05ed620
¨jzvolensky¨ <juro.zvolensky@gmail.com>
2023-09-04 12:12:28 +0000

    testing update

--- src-exp/hello_world.py
+++ src-exp/hello_world.py
@@ -3,6 +3,7 @@ import pystac
 import requests
 import json
 import yaml
+from datetime import datetime
 
 @click.command()
 @click.option("-f", "--file", type=click.Path(exists=True), default="params.yaml", help="Path to parameter file")
@@ -15,12 +16,24 @@ def main(file):
 
     catalog = pystac.Catalog(id="hello-catalog", description="Hello World Catalogue")
 
-    item = pystac.Item(id="my-item", geometry=None, bbox=None, datetime=None)
+    item = pystac.Item(id="my-item", 
+                    geometry=None, 
+                    datetime=datetime(1970,1,1,0,0,0), 
+                    bbox=None, 
+                    properties={
+                        "title": input_string  # Set the title to the input_string value
+                    }
+                )
     item.add_asset("data", pystac.Asset(href="http://example.com/data"))
 
     catalog.add_item(item)
 
-    click.echo(response.text)
+    # Print the entire catalog structure
+    catalog_dict = catalog.to_dict()
+    click.echo(json.dumps(catalog_dict, indent=4))
+    
+    # Access the title of the item
+    click.echo(f"Item Title: {item.common_metadata.title}")
 
 if __name__ == "__main__":
     main()
